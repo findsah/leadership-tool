cd616a4f8e8a34c9fe43ab214420bd927ef078fe
User <user@Users-MacBook-Pro-2.local>
2023-03-23 12:07:37 +0000

    new

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES= {
-        'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'membershipplatform',
-         'USER': 'postgres',
-         'PASSWORD': 'ilfaz786',
-         'HOST': '127.0.0.1',
-         'PORT': '5432',
-      }
-  }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# DATABASES= {
+#         'default': {
+#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#          'NAME': 'membershipplatform',
+#          'USER': 'postgres',
+#          'PASSWORD': 'ilfaz786',
+#          'HOST': '127.0.0.1',
+#          'PORT': '5432',
+#       }
+#   }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
